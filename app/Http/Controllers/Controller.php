0d608df722d46ef723eaefd156175eed8e5decde
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2024-03-13 23:29:44 +0700

    set error response on abstract class Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+
 abstract class Controller
 {
-    //
+    protected RedirectResponse $response;
+
+    /**
+     * Use to check is response from service error or not
+     *
+     * @param array $response
+     * @param string|null $redirectRoute
+     * @param array $params
+     * @return bool
+     */
+    protected function isError(array $response, string $redirectRoute = null, array $params = []): bool
+    {
+        if (!$response["success"]) {
+            if ($redirectRoute) {
+                $this->setErrorResponse(
+                    redirect()->route($redirectRoute, $params)->withErrors(["errors" => $response["message"]])->withInput()
+                );
+            } else {
+                $this->setErrorResponse(
+                    redirect()->back()->withErrors(["errors" => $response["message"]])->withInput()
+                );
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Use to set data response when response error
+     * @param RedirectResponse $response
+     * @return void
+     */
+    protected function setErrorResponse(RedirectResponse $response): void
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Use to redirect when error
+     * @return RedirectResponse
+     */
+    protected function getErrorResponse(): RedirectResponse
+    {
+        return $this->response;
+    }
 }
