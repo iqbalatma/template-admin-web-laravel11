559ae193162b94795be1078945bcb193bda944d6
Iqbal Atma Muliawan <iqbalatma@gmail.com>
2024-03-14 16:10:45 +0700

    role and permissin model

---
+++ app/Models/Permission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\Table;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+
+/**
+ * @property string id
+ * @property string name
+ * @property string guard_name
+ * @property string feature_group
+ * @property string description
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ */
+class Permission extends \Spatie\Permission\Models\Permission
+{
+    use HasUuids;
+
+    protected $table = Table::PERMISSIONS->value;
+
+    protected $fillable = [
+        "name", "guard_name", "feature_group", "description"
+    ];
+}
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Enums\Table;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasUuids;
+
+    protected $table = Table::ROLES->value;
 }
